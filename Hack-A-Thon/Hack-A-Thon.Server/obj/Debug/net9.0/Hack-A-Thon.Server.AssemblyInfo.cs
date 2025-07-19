1c52a510a0446a9b6fecd8cab232a749968a1f16
parrottjt <47727512+parrottjt@users.noreply.github.com>
2025-07-19 14:04:44 -0400

    Unit Test added for front end
    Used agent to create Unit Tests for front end. Achieved 100% cyclomatic test coverage.

--- Hack-A-Thon/Hack-A-Thon.Server/obj/Debug/net9.0/Hack-A-Thon.Server.AssemblyInfo.cs
+++ Hack-A-Thon/Hack-A-Thon.Server/obj/Debug/net9.0/Hack-A-Thon.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hack-A-Thon.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29adc9c203e7c3e4c1f02de6c7b0720ddf541dc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf85ff84f3be317ffa4fa7216343f093e6143c0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hack-A-Thon.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hack-A-Thon.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
